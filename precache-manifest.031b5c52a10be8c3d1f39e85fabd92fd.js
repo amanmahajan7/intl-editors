6859b93bf06c911f1b426c3e2975eff011c62551
Aman Mahajan <amahajan@stratag.com>
2019-01-14 12:45:46 -0600

    Updates

---
+++ precache-manifest.031b5c52a10be8c3d1f39e85fabd92fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "defe66f078f4d8326f12",
+    "url": "/intl-editors/static/css/main.00b1e6fc.chunk.css"
+  },
+  {
+    "revision": "defe66f078f4d8326f12",
+    "url": "/intl-editors/static/js/main.defe66f0.chunk.js"
+  },
+  {
+    "revision": "472d5e45357af3bebb1b",
+    "url": "/intl-editors/static/js/1.472d5e45.chunk.js"
+  },
+  {
+    "revision": "70790b2a884bbf58adaf",
+    "url": "/intl-editors/static/js/runtime~main.70790b2a.js"
+  },
+  {
+    "revision": "17e6ed7e04038d72b6e0ea1a442e2b07",
+    "url": "/intl-editors/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/intl-editors/precache-manifest.98e0914f38491f2ddd145943b764fcf7.js"
+  "/intl-editors/precache-manifest.031b5c52a10be8c3d1f39e85fabd92fd.js"
 );
 
 workbox.clientsClaim();
